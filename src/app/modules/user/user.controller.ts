ca6d4ce44f84354b2e6a10e6d0c3391fe7b1958a
Shazzadul-Shakib <shakib1186@gmail.com>
2025-10-23 15:18:23 +0600

    fix(auth): update cookie security settings for tokens

--- src/app/modules/user/user.controller.ts
+++ src/app/modules/user/user.controller.ts
@@ -25,15 +25,20 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
 
   // ----- set refresh token in cookie ----- //
   res.cookie("refreshToken", result.refreshToken, {
-    httpOnly: config.node_env === "production",
-    secure: true,
-    sameSite: config.node_env === "production" ? "none" : "strict",
+    httpOnly: true, 
+    secure: config.node_env === "production",
+    sameSite: config.node_env === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+    path: "/", 
   });
+
   // ----- set access token in cookie ----- //
   res.cookie("accessToken", result.accessToken, {
-    httpOnly: config.node_env === "production",
-    secure: true,
-    sameSite: config.node_env === "production" ? "none" : "strict",
+    httpOnly: true,
+    secure: config.node_env === "production",
+    sameSite: config.node_env === "production" ? "none" : "lax", 
+    maxAge: 15 * 60 * 1000,
+    path: "/",
   });
 
   sendResponse(res, {
